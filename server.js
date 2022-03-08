d9ce4e48dc1221f3085584c148ae14d5cddca321
connorhitchings <connorch@live.unc.edu>
2022-03-08 17:13:28 -0500

    First commit of a3, watching along with lecture

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+
+
+const server = app.listen(port, () => {
+    console.log('App listening on port %PORT'.replace('%PORT',port))
+});
+
+app.use(function(req,res) {
+    res.status(404).send("Endpoint does not exist.")
+    res.type("text/plain")
+})
