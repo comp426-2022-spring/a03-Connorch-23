6742d028be2c7572660f264ddd5ec47fc1d2ed9e
connorhitchings <connorch@live.unc.edu>
2022-03-10 11:19:20 -0500

    Main should be updated

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const server = app.listen(port, () => {
     console.log('App listening on port %PORT'.replace('%PORT',port))
 });
 
+app.get('/app', (req,res) => {
+    res.status(200).end("The API is working!")
+    res.type("text/plain")
+})
 
 app.use(function(req,res) {
     res.status( 404).send("Endpoint does not exist.")
