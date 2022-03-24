549c651abf5cf208fd1eee2a4a3ddac513873003
connorhitchings <connorch@live.unc.edu>
2022-03-24 16:01:11 -0400

    work

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express()
-const port = port || process.env.port || 3000
+const port = process.env.PORT || 3000
 /** Coin flip functions 
  * This module will emulate a coin flip given various conditions as parameters as defined below
  */
