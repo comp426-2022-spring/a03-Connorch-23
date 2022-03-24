61287d22ddc059d95572ccfef1956486fd2ab8ea
connorhitchings <connorch@live.unc.edu>
2022-03-24 15:47:58 -0400

    trying port 3000

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express()
-const port = 5000
+const port = 3000
 /** Coin flip functions 
  * This module will emulate a coin flip given various conditions as parameters as defined below
  */
